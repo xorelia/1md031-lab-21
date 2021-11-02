9ff630b6d87cbc56dadbdcd90e6c8f46119855f5
laaksoharju <mikael.laaksoharju@it.uu.se>
2021-11-02 15:24:22 +0100

    initializing lab

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 //UNCOMMENT THE BELOW LINES WHEN YOU REACH TO THE VUE PART OF THE TUTORIAL
 
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+// import { createApp } from 'vue'
+// import App from './App.vue'
+// import router from './router'
 
-createApp(App).use(router).mount('#app')
+// createApp(App).use(router).mount('#app')
 
